64c713e629702d184dd20b7ef205c450bed2d9d7
otatsyshyn <58274428+otatsyshyn@users.noreply.github.com>
2020-02-15 04:20:27 +0100

    Blog Tests updates

--- src/test/java/com/romanceabroad/ui/BlogTests.java
+++ src/test/java/com/romanceabroad/ui/BlogTests.java
@@ -17,55 +17,37 @@ public class BlogTests extends BaseUI {
         for (int i = 0; i < linksOfArticles.size() ; i++) {
             WebElement link = linksOfArticles.get(i);
             nameOfArticles = link.getText();
-            if (nameOfArticles.contains("How it works")){
 
-            }else
-            if (nameOfArticles.contains("Kharkov Dating & Marriage Agency")){
+            if (nameOfArticles.contains("Kharkov dating agency")){
 
-            }else
-            if (nameOfArticles.contains("Kiev marriage agency")){
+            }else if (nameOfArticles.contains("Kiev dating agency")){
 
-            }else
-            if (nameOfArticles.contains("Odessa Dating Agency")){
+            }else if (nameOfArticles.contains("Odessa dating agency")){
 
-            }else
-            if (nameOfArticles.contains("Mail Order Girls Catalog")){
+            }else if (nameOfArticles.contains("Mail order girls")){
 
-            }else
-            if (nameOfArticles.contains("Beautiful Ukrainian Girls Online")){
+            }else if (nameOfArticles.contains("Beautiful urkainian girls")){
 
-            }else
-            if (nameOfArticles.contains("Beautiful Brides in Ukraine")){
+            }else if (nameOfArticles.contains("Real Ukrainian brides")){
 
-            }else
-            if (nameOfArticles.contains("Eastern European Girls")){
+            }else if (nameOfArticles.contains("Eastern European women")){
 
-            }else
-            if (nameOfArticles.contains("Marriage And Dating Agency")){
+            }else if (nameOfArticles.contains("Marriage agency in Ukraine")){
 
-            }else
-            if (nameOfArticles.contains("Kiev Matchmaking Agency")){
+            }else if (nameOfArticles.contains("Kiev dating site")){
 
-            }else
-            if (nameOfArticles.contains("Find Ukrainian Girlfriend Online")){
+            }else if (nameOfArticles.contains("Find Ukrainian girlfriend")){
 
-            }else
-            if (nameOfArticles.contains("Slavic Lady for Marriage")){
+            }else if (nameOfArticles.contains("Slavic women for marriage")){
 
-            }else
-            if (nameOfArticles.contains("How To Marry Ukraine Women")){
+            }else if (nameOfArticles.contains("How to marry Ukrainian lady")){
 
-            }else
-            if (nameOfArticles.contains("Ukrainian dating website for singles")){
+            }else if (nameOfArticles.contains("Free Ukrainian dating site")){
 
-            }else
-            if (nameOfArticles.contains("9 Factors to Keep in Mind When Dating a Ukrainian Woman")){
+            }else if (nameOfArticles.contains("9 Factors to Keep in Mind When Dating a Ukrainian Woman")){
 
-            }else
-            if (nameOfArticles.contains("Is There a Difference Between Dating or Courting a Ukrainian Woman?")){
-
-            }
-            else {
+            }else if (nameOfArticles.contains("Is There a Difference Between Dating or Courting a Ukrainian Woman?"))
+            {
             link.click();
             titleArticle = blogPage.getAnyTitle();
             Assert.assertEquals(nameOfArticles, titleArticle);
